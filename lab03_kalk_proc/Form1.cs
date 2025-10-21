e0823d3e84bc3b0c758e68d80d237ba2c5865c8d
Paweł Niczyporuk <niczyporuk24080@stud.akademiabialska.pl>
2025-10-21 22:45:15 +0200

    Dodano klasę user_input zawierającą niezbędne obliczenia, dodano do Form1.cs możliwość przypisania danych wprowadzonych w GUI

--- lab03_kalk_proc/Form1.cs
+++ lab03_kalk_proc/Form1.cs
@@ -7,5 +7,16 @@ namespace lab03_kalk_proc
             InitializeComponent();
             this.Text = "Kalkulator procentowy";
         }
+        /*
+        double vol = double.Parse(pojemnosc_substancji_tutaj.Text);
+        double percent = double.Parse(procent_substancji_tutaj.Text);
+        int count = int.Parse(ilosc_naczym_tutaj.Text);
+
+        user_input input = new user_input(vol, percent, count);
+
+        tu_wstawisz_nazwe_pola_w_ktorym_bedzie_wyswietlany_wynik_obj_plynu.Text = input.Subst_sum().ToString();
+        tak_samo_jak_powyrzej_tylko_dla_samej_czystej_substancji.Text = input.Clean_subst_vol().ToString();
+        */
+        
     }
 }
---
+++ lab03_kalk_proc/user_input.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab03_kalk_proc
+{
+    internal class user_input
+    {
+        private double dish_vol = 0;
+        private double subst_percent = 0;
+        private int dish_numb = 0;
+
+
+        public user_input(double dish_vol, double subst_percent, int dish_numb)
+        {
+            this.dish_vol = dish_vol;
+            this.subst_percent = subst_percent;
+            this.dish_numb = dish_numb;
+        }
+
+        // Metoda do obliczenia łącznej objętości
+        public double Subst_sum()
+        {
+            return dish_vol * dish_numb;
+        }
+
+        // Metoda do obliczenia objętości czystej substancji
+        public double Clean_subst_vol()
+        {
+            return Subst_sum() * (subst_percent / 100);
+        }
+    }
+}
