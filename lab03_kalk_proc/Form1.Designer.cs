67b7dad3ec41cddeddc82308687b40fd80e4fbe8
Ignacy Semeniuk <semeniuk24162@stud.akademiabialska.pl>
2025-10-27 22:11:44 +0100

    quick fix

--- lab03_kalk_proc/Form1.Designer.cs
+++ lab03_kalk_proc/Form1.Designer.cs
@@ -33,39 +33,35 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            label4 = new Label();
             containerComboBox = new ComboBox();
-            label5 = new Label();
-            delButton = new Button();
-            conteinerAdd = new Button();
             label6 = new Label();
             label7 = new Label();
             totalVolume = new TextBox();
             subVolume = new TextBox();
             liquidComboBox = new ComboBox();
-            liquidAdd = new Button();
-            liquidDel = new Button();
             label8 = new Label();
-            label9 = new Label();
-            label10 = new Label();
             resButton = new Button();
             clearButton = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
             ((System.ComponentModel.ISupportInitialize)conteinerNUM).BeginInit();
             ((System.ComponentModel.ISupportInitialize)precentSub).BeginInit();
             SuspendLayout();
             // 
             // conteinerNUM
             // 
-            conteinerNUM.Location = new Point(191, 109);
+            conteinerNUM.Location = new Point(73, 153);
             conteinerNUM.Maximum = new decimal(new int[] { int.MaxValue, 0, 0, 0 });
             conteinerNUM.Name = "conteinerNUM";
-            conteinerNUM.Size = new Size(72, 23);
+            conteinerNUM.Size = new Size(68, 23);
             conteinerNUM.TabIndex = 0;
             conteinerNUM.ThousandsSeparator = true;
             // 
             // precentSub
             // 
-            precentSub.Location = new Point(200, 224);
+            precentSub.Location = new Point(221, 149);
             precentSub.Name = "precentSub";
             precentSub.Size = new Size(43, 23);
             precentSub.TabIndex = 1;
@@ -73,7 +69,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(73, 91);
+            label1.Location = new Point(98, 91);
             label1.Name = "label1";
             label1.Size = new Size(98, 15);
             label1.TabIndex = 2;
@@ -82,7 +78,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(196, 91);
+            label2.Location = new Point(73, 135);
             label2.Name = "label2";
             label2.Size = new Size(31, 15);
             label2.TabIndex = 3;
@@ -91,55 +87,19 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(200, 206);
+            label3.Location = new Point(219, 135);
             label3.Name = "label3";
             label3.Size = new Size(63, 15);
             label3.TabIndex = 4;
             label3.Text = "Ile procent";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(73, 144);
-            label4.Name = "label4";
-            label4.Size = new Size(83, 15);
-            label4.TabIndex = 5;
-            label4.Text = "Usuń naczynie";
             // 
             // containerComboBox
             // 
             containerComboBox.FormattingEnabled = true;
             containerComboBox.Location = new Point(73, 109);
             containerComboBox.Name = "containerComboBox";
-            containerComboBox.Size = new Size(112, 23);
+            containerComboBox.Size = new Size(140, 23);
             containerComboBox.TabIndex = 6;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(162, 144);
-            label5.Name = "label5";
-            label5.Size = new Size(87, 15);
-            label5.TabIndex = 7;
-            label5.Text = "Dodaj naczynie";
-            // 
-            // delButton
-            // 
-            delButton.Location = new Point(73, 162);
-            delButton.Name = "delButton";
-            delButton.Size = new Size(83, 23);
-            delButton.TabIndex = 8;
-            delButton.Text = "Usuń";
-            delButton.UseVisualStyleBackColor = true;
-            // 
-            // conteinerAdd
-            // 
-            conteinerAdd.Location = new Point(162, 162);
-            conteinerAdd.Name = "conteinerAdd";
-            conteinerAdd.Size = new Size(87, 23);
-            conteinerAdd.TabIndex = 9;
-            conteinerAdd.Text = "Dodaj";
-            conteinerAdd.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
@@ -178,55 +138,19 @@
             // liquidComboBox
             // 
             liquidComboBox.FormattingEnabled = true;
-            liquidComboBox.Location = new Point(73, 223);
+            liquidComboBox.Location = new Point(221, 109);
             liquidComboBox.Name = "liquidComboBox";
             liquidComboBox.Size = new Size(121, 23);
             liquidComboBox.TabIndex = 14;
             // 
-            // liquidAdd
-            // 
-            liquidAdd.Location = new Point(162, 273);
-            liquidAdd.Name = "liquidAdd";
-            liquidAdd.Size = new Size(87, 23);
-            liquidAdd.TabIndex = 17;
-            liquidAdd.Text = "Dodaj";
-            liquidAdd.UseVisualStyleBackColor = true;
-            // 
-            // liquidDel
-            // 
-            liquidDel.Location = new Point(73, 273);
-            liquidDel.Name = "liquidDel";
-            liquidDel.Size = new Size(83, 23);
-            liquidDel.TabIndex = 18;
-            liquidDel.Text = "Usuń";
-            liquidDel.UseVisualStyleBackColor = true;
-            // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(73, 205);
+            label8.Location = new Point(219, 91);
             label8.Name = "label8";
             label8.Size = new Size(92, 15);
             label8.TabIndex = 19;
             label8.Text = "Wybierz roztwór";
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Location = new Point(73, 255);
-            label9.Name = "label9";
-            label9.Size = new Size(77, 15);
-            label9.TabIndex = 20;
-            label9.Text = "Usuń roztwór";
-            // 
-            // label10
-            // 
-            label10.AutoSize = true;
-            label10.Location = new Point(162, 255);
-            label10.Name = "label10";
-            label10.Size = new Size(81, 15);
-            label10.TabIndex = 21;
-            label10.Text = "Dodaj roztwór";
             // 
             // resButton
             // 
@@ -246,33 +170,62 @@
             clearButton.Text = "Wyczyść";
             clearButton.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(73, 193);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(140, 23);
+            textBox1.TabIndex = 24;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(221, 193);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(121, 23);
+            textBox2.TabIndex = 25;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(73, 175);
+            label4.Name = "label4";
+            label4.Size = new Size(128, 15);
+            label4.TabIndex = 26;
+            label4.Text = "Objętość naczynia (ml)";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(219, 175);
+            label5.Name = "label5";
+            label5.Size = new Size(121, 15);
+            label5.TabIndex = 27;
+            label5.Text = "Stężenie roztworu (%)";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(clearButton);
             Controls.Add(resButton);
-            Controls.Add(label10);
-            Controls.Add(label9);
             Controls.Add(label8);
-            Controls.Add(liquidDel);
-            Controls.Add(liquidAdd);
             Controls.Add(liquidComboBox);
             Controls.Add(subVolume);
             Controls.Add(totalVolume);
             Controls.Add(label7);
             Controls.Add(label6);
-            Controls.Add(conteinerAdd);
-            Controls.Add(delButton);
-            Controls.Add(label5);
             Controls.Add(containerComboBox);
-            Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(precentSub);
             Controls.Add(conteinerNUM);
+            DoubleBuffered = true;
             Name = "Form1";
             Text = "Kalkulator procentowy";
             ((System.ComponentModel.ISupportInitialize)conteinerNUM).EndInit();
@@ -288,22 +241,18 @@
         private Label label1;
         private Label label2;
         private Label label3;
-        private Label label4;
         private ComboBox containerComboBox;
-        private Label label5;
-        private Button delButton;
-        private Button conteinerAdd;
         private Label label6;
         private Label label7;
         private TextBox totalVolume;
         private TextBox subVolume;
         private ComboBox liquidComboBox;
-        private Button liquidAdd;
-        private Button liquidDel;
         private Label label8;
-        private Label label9;
-        private Label label10;
         private Button resButton;
         private Button clearButton;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
     }
 }
